473621909e8ee2b6a9b0533360755ca0caeba522
htl <hoangthanhlong@gmail.com>
2018-12-04 15:44:49 +0700

    edit day1, use set to speed up

--- day1.py
+++ day1.py
@@ -7,12 +7,14 @@ from itertools import cycle
 with open('day1_input.txt') as f:
     lines = f.readlines()
 changes = cycle(int(line) for line in lines)
-freqs = [0]
+last_freq = 0
+freqs = set()
 
 for c in changes:
-    new_freq = freqs[-1] + c
+    new_freq = last_freq + c
     if new_freq in freqs:
         print(new_freq)
         break
     else:
-        freqs.append(new_freq)
+        freqs.add(new_freq)
+        last_freq = new_freq
